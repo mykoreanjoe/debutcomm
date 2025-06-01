29a8aa7ad1c84bba2e1602c3e199d934ae10b839
mykoreanjoe <mykoreanjoe@gmail.com>
2025-06-01 19:49:58 +0900

    빌드 오류 수정: CurriculumCard를 클라이언트 컴포넌트로 명시

--- app/curriculum/page.tsx
+++ app/curriculum/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import React from 'react';
 import { Button } from '@/components/ui/button';
 import { MessageSquare, BookOpen, Rocket, GraduationCap, Target, LucideIcon } from 'lucide-react';
